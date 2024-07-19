ee6709b82dd7fdffaa0b42f70b6c5904528480c9
t.chambard <t.chambard@bee-buzziness.com>
2024-07-19 15:13:53 +0200

    feat: add seeds for monitoring and repair db:seed script

--- apps/web/src/database/schema.ts
+++ apps/web/src/database/schema.ts
@@ -1,4 +1,17 @@
 import { pgTable, serial, text } from "drizzle-orm/pg-core";
+
+export type Monitoring = {
+	id: number;
+	name: string;
+	eventAbi: string;
+	contractAddress: string;
+};
+
+export type Event = {
+	monitoringId: string;
+	transactionHash: string;
+};
+
 export const monitoring = pgTable("monitoring", {
 	id: serial("id").primaryKey(),
 	name: text("name"),
