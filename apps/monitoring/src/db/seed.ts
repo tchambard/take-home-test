5f3c5759570bff2bc406da11eb4e45e2d25a7413
t.chambard <t.chambard@bee-buzziness.com>
2024-07-24 16:00:49 +0200

    fix(seeds): sync monitoring name with its id

--- apps/monitoring/src/db/seed.ts
+++ apps/monitoring/src/db/seed.ts
@@ -1,36 +1,36 @@
-import { randomBytes } from 'crypto';
+import { randomBytes } from "node:crypto";
 
 import { db } from "./db";
-import { monitoring, event } from "./schema";
+import { event, monitoring } from "./schema";
 
 function generateRandomHash(): string {
-	return `0x${randomBytes(32).toString('hex')}`;
+	return `0x${randomBytes(32).toString("hex")}`;
 }
 const run = async () => {
 	// insert monitoring
-	for (let i = 0; i < 40; i++) {
-		await db.insert(monitoring)
-			.values({
-				contractAddress: generateRandomHash(),
-				eventAbi: '{}',
-				name: `Monitoring ${i}`
-			});
+	for (let i = 1; i < 40; i++) {
+		await db.insert(monitoring).values({
+			contractAddress: generateRandomHash(),
+			eventAbi: "{}",
+			name: `Monitoring ${i}`,
+		});
 	}
 
 	// insert monitoring
 	for (let i = 0; i < 300; i++) {
 		const monitoringId = Math.floor(Math.random() * 40) + 1;
-		await db.insert(event)
-			.values({
-				monitoringId,
-				transactionHash: generateRandomHash(),
-			});
+		await db.insert(event).values({
+			monitoringId,
+			transactionHash: generateRandomHash(),
+		});
 	}
 };
 
-run().then(() => {
-	process.exit(0);
-}).catch((e) => {
-	console.error("An error occurred while inserting data seeds", e.stack);
-	process.exit(1);
-});
+run()
+	.then(() => {
+		process.exit(0);
+	})
+	.catch((e) => {
+		console.error("An error occurred while inserting data seeds", e.stack);
+		process.exit(1);
+	});
